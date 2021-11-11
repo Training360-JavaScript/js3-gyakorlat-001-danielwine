c6d8ce25c822f62370ddf977b7c533d1dc5eca6c
daniel <danielwine@tutamail.com>
2021-11-11 16:18:48 +0100

    All done.

--- solutions/app2.js
+++ solutions/app2.js
@@ -1,7 +1,20 @@
 
+const HU = {
+    locale: 'HU-hu',
+    currencyLocale: 'HUF',
+    date(date) {
+        return new Intl.DateTimeFormat(this.locale).format(date)
+    },
+    curreny(price) {
+        return new Intl.NumberFormat(this.locale, {
+            style: 'currency', currency: this.currencyLocale
+        }).format(price)
+    },
+    list(list) {
+        return new Intl.ListFormat(this.locale, {
+            style: 'long', type: 'conjunction'
+        }).format(list)
+    }
+}
 
-
-
-
-
-// export default HU
+export default HU;
